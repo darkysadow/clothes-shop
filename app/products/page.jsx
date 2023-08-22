d24e2e21a1c79fc8b27867465b6ec9754c65f0c4
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-08-22 16:16:06 +0300

    Created Products page, products fetched and pasted to this page from Shopify

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,7 +1,11 @@
-export default function Products() {
+import ProductCard from "@/components/ProductCard/ProductCard";
+import { getAllProducts, getAllProductsInCollection } from "@/lib/shopify";
+
+export default async function Products() {
+    const products = await getAllProducts()
     return (
-        <div className="container mx-auto">
-            
+        <div className="container mx-auto flex flex-row flex-wrap justify-between items-start p-2 gap-y-5">
+            {products.map(product => <ProductCard product={product} />)}
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -5,7 +5,17 @@ const nextConfig = {
         API_KEY: process.env.API_KEY,
         API_SECRET_KEY: process.env.API_SECRET_KEY,
         DOMAIN: process.env.DOMAIN
-    }
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.shopify.com',
+            port: '',
+            pathname: '/s/files/1/0811/2072/3233/files/**',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
