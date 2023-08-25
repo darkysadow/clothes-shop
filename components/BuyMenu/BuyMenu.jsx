7c08ce30184d0f453be4d89dd9d655e62eed7e97
Oleksandr Poberezhniak <alexsmithshow@gmail.com>
2023-08-25 17:29:15 +0300

    Fixed bug with undefined when user pick first size

--- components/BuyMenu/BuyMenu.jsx
+++ components/BuyMenu/BuyMenu.jsx
@@ -1,29 +1,37 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export default function BuyMenu({ variants, options }) {
-    const [selectedVariant, setSelectedVariant] = useState(variants[0]);
-    
-    const handleOptionClick = (optionName, optionValue) => {
-        const newVariant = { ...selectedVariant };
-        /* const variantIndex = variants.find((opt) => opt.selectedOptions.value === optionValue && opt.selectedOptions.name === optionName); */
-        const optionIndex = newVariant.selectedOptions.findIndex((opt) => opt.name === optionName);
-        if (optionIndex !== -1) {
-          newVariant.price = variants.find(opt => opt.selectedOptions[optionIndex].value === optionValue).price
-          newVariant.selectedOptions[optionIndex].value = optionValue;
-        }
-        setSelectedVariant(newVariant);
-      };
+  const [selectedVariant, setSelectedVariant] = useState(variants[0]);
+  useEffect(() => {
+    console.log(selectedVariant);
+  }, [selectedVariant])
+  const handleOptionClick = (optionName, optionValue) => {
+    const newSelectedOptions = selectedVariant.selectedOptions.map((opt) =>
+      opt.name === optionName ? { ...opt, value: optionValue } : opt
+    );
 
-      console.log(variants.find(opt => opt.selectedOptions[0].value === selectedVariant.selectedOptions[0].value))
+    const matchingVariant = variants.find((variant) =>
+      variant.selectedOptions.every((opt) =>
+        newSelectedOptions.some((newOpt) => newOpt.name === opt.name && newOpt.value === opt.value)
+      )
+    );
 
+    if (matchingVariant) {
+      setSelectedVariant(matchingVariant);
+    }
+  };
     return (
         <div className="flex flex-col">
-            <h2>Price</h2>
-            {selectedVariant.price.amount + " " + selectedVariant.price.currencyCode}
+            {selectedVariant.availableForSale ?
+                <><h2>Price</h2>
+                <div>{selectedVariant.price.amount + " " + selectedVariant.price.currencyCode}</div></>
+                : <h2>Not available</h2>
+            }
+            
             <h2>Variants</h2>
             {options.map((option, index) => <div key={index} className="space-y-0">
                 <h2 className="text-xl font-medium my-2">{option.name}</h2>
@@ -32,12 +40,11 @@ export default function BuyMenu({ variants, options }) {
                         <div className={twMerge(
                             "hover:scale-[1.05] rounded-lg bg-slate-500 text-white border-slate-400 border-2 px-4 py-1",
                             value === selectedVariant.selectedOptions.find((opt) => opt.name === option.name)?.value &&
-                            "bg-black",
+                            "bg-black hover:scale-1",
                             
-
                         )
                             }
-                            onClick={() => handleOptionClick(option.name, value)}>
+                        onClick={() => handleOptionClick(option.name, value)}>
                             {value}
                         </div>
                     </div>))}
